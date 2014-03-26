2059127b87c13122c18c0649a0399a266e7fc8b4
Niclas Sahlin <niclas@niclassahlin.com>
2014-03-26 21:13:25 +0100

    Added Nodemon

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,19 @@ module.exports = function(grunt){
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		nodemon: {
+			dev:{
+				script: 'server.js',
+				options: {
+					env: {
+						PORT: 8888
+					},
+					ext: 'js'
+				}
+			}
+		}
 	});
 
-	grunt.registerTask('default', []);
+	grunt.registerTask('default', ['nodemon']);
 };
